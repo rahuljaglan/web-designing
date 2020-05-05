411853e3d45dfe11fd6d307e9d2fa858812f462d
rahuljaglan <54793387+rahuljaglan@users.noreply.github.com>
2020-05-05 10:52:36 +0530

    Add files via upload

---
+++ qus3.html.php
@@ -0,0 +1,21 @@
+
+<html>
+    <head>
+        <title>Square Function</title>
+        <script>
+            function square()
+            {
+                for(var x=1;x<=50;x++)
+                    {
+                        document.write("Square of " + x + " : " + x*x + "<br>");
+                    }
+            }
+        </script>
+    </head>
+    
+    <body>
+        <script>          
+            square();
+        </script>
+    </body>
+</html>
---
+++ qus5.html.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Lab Work</title>
+    </head>
+    
+    <body>
+        <script>
+            function addNum(first,second)
+            {
+                var value = "Result is : ";
+                function add()
+                {
+                    return value + (first + second);
+                }
+                return add();
+            }
+            var a=addNum(5,6);
+            document.write("a = "+ a);
+        </script>
+    </body>
+</html>
